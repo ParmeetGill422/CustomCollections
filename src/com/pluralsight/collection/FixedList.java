d6f184604745a6f8abc06acf241d944787da9c9e
ParmeetGill422 <parmeetgill422@gmail.com>
2025-05-22 00:05:33 +0530

    first

--- src/com/pluralsight/collection/FixedList.java
+++ src/com/pluralsight/collection/FixedList.java
@@ -1,4 +1,25 @@
 package com.pluralsight.collection;
 
-public class FixedList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixedList<T> {
+    private List<T> items;
+    private int maxSize;
+
+    public FixedList(int maxSize) {
+        this.maxSize = maxSize;
+        this.items = new ArrayList<>();
+    }
+
+    public void add(T item) {
+        if (items.size() >= maxSize) {
+            throw new IllegalStateException("List is full. Max size is " + maxSize);
+        }
+        items.add(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
 }
